3454109112c14ebf33279fddff7b40ec14139360
Elianehbmna <elianehabimana3@gmail.com>
2019-09-29 21:14:07 +0200

    subscribe method m=n views

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,6 @@ from ..models import User,Blog
 class PitchForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
     description = TextAreaField("What would you like to pitch ?",validators=[Required()])
-    # category = RadioField('Label', choices=[ ('Educational','Educational'), ('Musical','Musical'),('Religion','Religion'),('Comedy','Comedy')],validators=[Required()])
     submit = SubmitField('Submit')
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
