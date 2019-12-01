09b8afd1d989688ecf9ff5957de5aea8d9ac6578
MATHEWS_JUMBA <mathewsjumba19@outlook.com>
2019-12-01 23:12:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
 
     QUOTES_API = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:maman@localhost/blog'
+    SECRET_KEY = os.environ.get('wefrwefdsc34343')
+    SQLALCHEMY_DATABASE_URI = 'postgres://vyvqzszajrmrui:b082c9f3c61b7ac2836067a8ea395c8a20c8723e96b91b79c812257d3b000c5b@ec2-54-221-214-183.compute-1.amazonaws.com:5432/d2hgsc2qvoielo'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -18,10 +18,10 @@ class ProdConfig(Config):
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:maman@localhost/watchlist_test'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vyvqzszajrmrui:b082c9f3c61b7ac2836067a8ea395c8a20c8723e96b91b79c812257d3b000c5b@ec2-54-221-214-183.compute-1.amazonaws.com:5432/d2hgsc2qvoielo'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:maman@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vyvqzszajrmrui:b082c9f3c61b7ac2836067a8ea395c8a20c8723e96b91b79c812257d3b000c5b@ec2-54-221-214-183.compute-1.amazonaws.com:5432/d2hgsc2qvoielo'
     DEBUG = True
 
 config_options = {
